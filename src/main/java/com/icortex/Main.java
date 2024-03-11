94497681f2a1181355db58bf8f537099654580fc
mahinhasan <mahinhasanaiub@gmail.com>
2024-03-11 17:34:53 +0600

    new fild

--- src/main/java/com/icortex/Main.java
+++ src/main/java/com/icortex/Main.java
@@ -3,5 +3,6 @@ package com.icortex;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Let's build the world !");
     }
 }
